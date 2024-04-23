8381917c5f7b45d02cb14830d345974f86c2f684
yinzs <1123944317@qq.com>
2024-04-23 13:24:53 +0800

    feat: Modify the drop list

--- apps/builder/src/views/tableBuilder/builder/index.vue
+++ apps/builder/src/views/tableBuilder/builder/index.vue
@@ -1,8 +1,45 @@
 <script lang="ts" setup>
 import RnConditions from './block/condition.vue'
-
+import {useSetDialog} from './block/setDialog'
+import {useColumnCondition} from ":/views/tableBuilder/builder/block/ColumnCondition";
+import {unionBy} from "lodash-es";
+const columnCondition = useColumnCondition()
+const setDialog = useSetDialog()
 const active = ref(false)
 const show = ref(false)
+const list=ref([])
+
+function editQuery(row) {
+  setDialog.open({
+    type: 1, set(row) {
+
+    }
+  })
+}
+
+function editScreen(row) {
+  setDialog.open({
+    type: 2, set(row) {
+
+    }
+  })
+}
+
+function addScreen(){
+  columnCondition.use({
+   columns: [{name: 'name', label: '姓名', visible: true}, {name: 'age', label: '年龄', visible: true}, {
+     name: 'sex',
+     label: '性别',
+     visible: true
+   }], save(cols) {
+     const selectArry = cols.filter(v => v.visible)
+     list.value = [...unionBy(list.value, selectArry, 'name')]
+   }
+ })
+}
+
+watch(()=>list.value,(val)=>{
+})
 </script>
 
 <template>
@@ -34,21 +71,21 @@ const show = ref(false)
             <n-space vertical>
               <n-space justify="space-between">
                 <label>是否启用查询</label>
-                <n-switch v-model:value="active" />
+                <n-switch v-model:value="active"/>
               </n-space>
               <n-space justify="space-between">
                 <label>是否启用筛查</label>
-                <n-switch v-model:value="active" />
+                <n-switch v-model:value="active"/>
               </n-space>
             </n-space>
             <div class="my-10px">
               查询条件
             </div>
-            <RnConditions :button-type="1" />
+            <RnConditions @update="editQuery"/>
             <div class="my-10px">
               筛查条件
             </div>
-            <RnConditions button-name="添加筛查条件" :button-type="2" />
+            <RnConditions v-model="list" button-name="添加筛查条件" @update="editScreen" @add="addScreen"/>
           </n-tab-pane>
           <n-tab-pane name="colunmn" tab="表格列设置">
             表格列设置
