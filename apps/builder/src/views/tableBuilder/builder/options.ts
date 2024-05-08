38e632bab77e7d60825f95f2bf88cb8774c82d14
wangxd <lovingskymm@foxmail.com>
2024-05-08 09:17:49 +0800

    fix: table update logic

--- apps/builder/src/views/tableBuilder/builder/options.ts
+++ apps/builder/src/views/tableBuilder/builder/options.ts
@@ -1,6 +1,4 @@
-import {
-  designerVoCreator }
-  from '@runafe/unified-api-designer'
+import { designerVoCreator } from '@runafe/unified-api-designer'
 import { clone } from '@monan/shared'
 import { tableSchema } from './tableSchema'
 import { filterableFields, viewModel } from './viewModels'
@@ -9,22 +7,32 @@ import type { CriteriaMeta } from ':/components/criterias'
 
 export const criteriasFields = ref<CriteriaMeta[]>([])
 
-watch(() => viewModel.value.code, () => {
-  const fields = clone(filterableFields.value) as unknown as CriteriaMeta[]
-  const vo = designerVoCreator(http, tableSchema.value.dataSource.serverName, tableSchema.value.dataSource.viewName)
-  fields.forEach(async (v) => {
-    if (v.type === 'ENUM') {
-      const { backData } = await vo.enum({ fieldCode: v.name })()
-      if (backData) {
-        v.valueOptions = backData as unknown as CriteriaMeta[]
+watch(
+  () => viewModel.value.code,
+  () => {
+    const fields = clone(filterableFields.value) as unknown as CriteriaMeta[]
+    const vo = designerVoCreator(
+      http,
+      tableSchema.value.dataSource.serverName,
+      tableSchema.value.dataSource.viewName,
+    )
+    fields.forEach(async (v) => {
+      if (v.type === 'ENUM') {
+        const { backData } = await vo.enum({ fieldCode: v.name })()
+        if (backData) {
+          v.valueOptions = backData as unknown as CriteriaMeta[]
+        }
       }
-    }
-    else if (v.selectable) {
-      const { backData } = await vo.query({ fieldCode: v.name })()
-      if (backData) {
-        v.valueOptions = backData as unknown as CriteriaMeta[]
+      else if (v.selectable) {
+        const { backData } = await vo.options({
+          fieldCode: v.name,
+          optionQuery: {},
+        })()
+        if (backData) {
+          v.valueOptions = backData as unknown as CriteriaMeta[]
+        }
       }
-    }
-  })
-  criteriasFields.value = fields
-})
+    })
+    criteriasFields.value = fields
+  },
+)
